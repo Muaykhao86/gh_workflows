50782289c79f0b6395e650c907187d45338926c9
Muaykhao86 <danielhardiman@live.co.uk>
2025-02-19 12:17:40 +0000

    add test lambdas

---
+++ lambdas/node/index.mjs
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch';
+
+export const handler = async (event) => {
+    const response = await fetch('https://api.github.com');
+    const data = await response.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello from Lambda!',
+            github_status: data
+        })
+    };
+};
